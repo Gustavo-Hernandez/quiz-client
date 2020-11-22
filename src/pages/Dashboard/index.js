ab7af304ebfa8f2b6e4461919ffc98293d4fc5f3
Gustavo Hernández <47768780+Gustavo-Hernandez@users.noreply.github.com>
2020-11-21 19:03:58 -0600

    Added Dashboard Background

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -15,7 +15,10 @@ import Chat from "./Chat";
 const useStyles = makeStyles((theme) => ({
   root: {
     height: "100vh",
-    backgroundColor: "#fafafa",
+    backgroundColor: "white",
+    backgroundImage:`url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='100%25' height='100%25' %3E%3Cdefs%3E%3ClinearGradient id='a' x1='0' x2='0' y1='0' y2='1'%3E%3Cstop offset='0' stop-color='%237a0099'/%3E%3Cstop offset='1' stop-color='%23100399'/%3E%3C/linearGradient%3E%3C/defs%3E%3Cpattern id='b' width='24' height='24' patternUnits='userSpaceOnUse'%3E%3Ccircle fill='%23ffffff' cx='12' cy='12' r='12'/%3E%3C/pattern%3E%3Crect width='100%25' height='100%25' fill='url(%23a)'/%3E%3Crect width='100%25' height='100%25' fill='url(%23b)' fill-opacity='0.1'/%3E%3C/svg%3E")`,
+    backgroundAttachment: "fixed",
+    backgroundSize: "cover",
   },
   buttonContainer: {
     width: "100%",
@@ -70,7 +73,7 @@ const Dashboard = () => {
   };
 
   const handleMessage = (value) => {
-    sendMessage(session.user.username,value,session.pin);
+    sendMessage(session.user.username, value, session.pin);
   };
 
   const open = Boolean(anchorEl);
@@ -123,7 +126,11 @@ const Dashboard = () => {
           horizontal: "center",
         }}
       >
-        <Chat messages={chatMessages} sendChatMessage={handleMessage} username={session.user.username} />
+        <Chat
+          messages={chatMessages}
+          sendChatMessage={handleMessage}
+          username={session.user.username}
+        />
       </Popover>
       <Fab
         color="secondary"
